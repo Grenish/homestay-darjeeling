eda5f9c402e0186fb262f8341a75436d0168986f
Grenish rai <ariestheracer@gmail.com>
2024-01-23 09:19:11 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Footer from "./Footer";
+import Gallery from "./Gallery";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Rooms from "./Rooms";
+
+export { About, Footer, Gallery, Navbar, Rooms, Hero };

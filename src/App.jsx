efc4d8f2349ed716874342450c02ec32275fcc61
Grenish rai <ariestheracer@gmail.com>
2024-01-23 09:18:53 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as components from "./components/index"
+
+const App = () => {
+  return (
+    <div>
+      <components.Navbar />
+      <components.Hero />
+      <components.Gallery />
+      <components.Footer />
+    </div>
+  );
+};
+
+export default App;

4a06908918db7992d3c4aca7d6344a5868630a37
Grenish rai <ariestheracer@gmail.com>
2024-01-24 16:37:00 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
 import ADs from "./ADsLodge.png";
+import Ads2 from "./ADs1-min.jpg";
+import Ads3 from "./ADs2-min.jpg";
+import Ads4 from "./ADs3-min.jpg";
+import Ads5 from "./ADs4-min.jpg";
 
-export { ADs };
+export { ADs, Ads2, Ads3, Ads4, Ads5 };

f8225567fa2a596440ee33a0b0f8c3cd08ad7052
Grenish rai <ariestheracer@gmail.com>
2024-01-24 16:37:08 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import About from "./About";
+import Booking from "./Booking";
 import Footer from "./Footer";
 import Gallery from "./Gallery";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Rooms from "./Rooms";
 
-export { About, Footer, Gallery, Navbar, Rooms, Hero };
+export { About, Footer, Gallery, Navbar, Rooms, Hero, Booking };
